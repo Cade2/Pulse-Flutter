a20d46596e7221f0883403366d69e71a0c76bcba
Cade <st10443998@vcconnect.edu.za>
2026-04-03 23:23:10 +0200

    Initialize Firebase Core in Pulse Flutter

--- pulse_flutter/lib/main.dart
+++ pulse_flutter/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pulse_flutter/app/app.dart';
+import 'package:pulse_flutter/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: PulseApp()));
 }
